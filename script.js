0c39022329c5b1205f721521bc98d9795a1c8f4c
mayameape <59280026+mayameape@users.noreply.github.com>
2020-10-29 14:03:20 +0530

    lastchange

--- script.js
+++ script.js
@@ -4,11 +4,13 @@ $("video").css("opacity","0.5");
 
 $("video").on("mouseenter",function(){
 	$(this).trigger("play");
+	$(this).muted = true;
       $(this).autoplay = true;
       $(this).css("opacity","1");
 });
 
 $("video").on("mouseleave",function(){
+	$(this).muted = true;
 	$(this).trigger("pause");
       $(this).autoplay = false;
       $(this).css("opacity","0.5");
